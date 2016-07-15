a869d686e8bc731601f54530f65dc8cb6ee96ac7
baton4eg <baton4eg@mail.ru>
2016-07-15 23:24:30 +0300

    Update functions.php

--- your-clean-template-3/functions.php
+++ your-clean-template-3/functions.php
@@ -5,6 +5,58 @@
  * @subpackage your-clean-template-3
  */
 
+/**------------------------------------------------
+Увеличим ширину блоков NGG в админке
+------------------------------------------------**/
+add_action('admin_head', 'my_custom_field');
+function my_custom_field() {
+  echo '<style>
+#ngg-listimages .column-5 textarea {
+    min-height: 315px;
+}
+  </style>';
+}
+/**------------------------------------------------
+Добавим классы кнопкам вперед назад в новостях
+------------------------------------------------**/
+add_filter('next_post_link', 'post_link_attributes');
+add_filter('previous_post_link', 'post_link_attributes');
+ 
+function post_link_attributes($output) {
+    $code = 'class="btn btn-default btn-block"';
+    return str_replace('<a href=', '<a '.$code.' href=', $output);
+}
+/**------------------------------------------------
+Вырезка текста для новостей
+использование:
+echo excerpt(30); //30 кол-во символов
+------------------------------------------------**/
+function excerpt($limit) {
+  $excerpt = explode(' ', get_the_excerpt(), $limit);
+  if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt).'...';
+  } else {
+    $excerpt = implode(" ",$excerpt);
+  }   
+  $excerpt = preg_replace('`\[[^\]]*\]`','',$excerpt);
+  return $excerpt;
+}
+function content($limit) {
+  $content = explode(' ', get_the_content(), $limit);
+  if (count($content)>=$limit) {
+    array_pop($content);
+    $content = implode(" ",$content).'...';
+  } else {
+    $content = implode(" ",$content);
+  }   
+  $content = preg_replace('/\[.+\]/','', $content);
+  $content = apply_filters('the_content', $content);
+  $content = str_replace(']]>', ']]&gt;', $content);
+  return $content;
+}
+
+
 add_theme_support('title-tag'); // теперь тайтл управляется самим вп
 
 register_nav_menus(array( // Регистрируем 2 меню
